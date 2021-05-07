043fdce612d8da73fe58a5211ab8051916eb3c82
arohiabhilasha <arohiabhilasha@gmail.com>
2021-05-07 13:37:08 +0530

    --- Auto-Git Commit ---

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask("tsf_b")
+app.config['DEBUG'] = True
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run()

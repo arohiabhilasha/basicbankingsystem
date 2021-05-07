62e489f9b68df67b6749ba46133cf4bcc189d4e5
arohiabhilasha <arohiabhilasha@gmail.com>
2021-05-07 23:04:32 +0530

    Customers details updated

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask("tsf_b")
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/customer')
-def index():
-    return render_template("customer.html")
+def cust():
+    customer = {"Abhinav":3000, "Ritul":50000, "Abhilasha": 20000, "Vaibhav": 12000}
+    return render_template("customer.html", arrayx=customer)
 
 app.run()
